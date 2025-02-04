2443bb359ec1050be2c066dd994dd08be95fec2f
@racascou <rafaelcastrocouto@gmail.com>
2025-02-04 13:36:13 -0300

    folha solicita

--- templates/Alunos/folhasolicita.php
+++ templates/Alunos/folhasolicita.php
@@ -23,16 +23,13 @@ if ($user_session) { $categoria_id = $user_session->get('categoria_id'); }
 echo $this->Form->create(null, ['class' => 'form-inline']);
 
 if ($categoria_id != 2) {
-    echo $this->Form->input('registro', ['type' => 'text', 'div' => 'form-group row','label' => ['text' => 'DRE', 'class' => 'label-control col-1'], 'placeholder' => 'Digite o DRE', 'between' => '<div class ="form-inline col-8">', 'after' => '</div>','class' => 'form-control required']);
+    echo $this->Form->input('registro', ['type' => 'text', 'div' => 'form-group row','label' => ['text' => 'DRE', 'class' => 'label-control col-1'], 'placeholder' => 'Digite o DRE', 'class' => 'form-control required']);
 } else {
-    echo $this->Form->input('registro', ['type' => 'text', 'div' => 'form-group row','label' => ['text' => 'DRE', 'class' => 'label-control col-1'], 'value' => $this->Session->read('numero'), 'between' => '<div class ="form-inline col-8">', 'after' => '</div>','class' => 'form-control required']);
+    echo $this->Form->input('registro', ['type' => 'text', 'div' => 'form-group row','label' => ['text' => 'DRE', 'class' => 'label-control col-1'], 'value' => $this->Session->read('numero'), 'class' => 'form-control required']);
 }
+
+echo $this->Form->submit('Confirma', ['type' => 'Submit', 'label' => ['text' => 'Confirma'], 'class' => 'button']);
+echo $this->Form->end();
+
 ?>
-<div class='row justify-content-left'>
-    <div class='col-auto'>
-        <?php
-        echo $this->Form->submit('Confirma', ['type' => 'Submit', 'label' => ['text' => 'Confirma', 'class' => 'col-4'], 'class' => 'btn btn-primary']);
-        echo $this->Form->end();
-        ?>
-    </div>
 </div>
